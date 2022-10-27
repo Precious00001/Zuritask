2d357996735c3974f4ca88de617dcb2743ba3352
Precious00001 <lanremidelola@gmail.com>
2022-10-27 05:03:38 +0100

    Practise

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=400)
+    date_released = models.CharField(max_length= 40)
+    likes = models.CharField(max_length= 40)
+    artiste_id = models.CharField(max_length= 40)
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=400)
+    song_id = models.CharField(max_length= 400)
